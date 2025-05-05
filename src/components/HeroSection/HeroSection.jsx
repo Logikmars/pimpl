d51ed6838c967c436c2648618cee64cd80c65014
ivandalechenko <ivandalechenko@gmail.com>
2025-05-05 18:39:23 +0300

    fix

--- src/components/HeroSection/HeroSection.jsx
+++ src/components/HeroSection/HeroSection.jsx
@@ -1,18 +1,18 @@
 import './HeroSection.scss';
-export default () => {return (
-    <div className='HeroSection container'>
-        <div className='HeroSection_decor'>
-            <div className='HeroSection_decor_container free_img'>
-                <div className='HeroSection_decor_img free_img'>
+export default () => {
+    return (
+        <div className='HeroSection container'>
+            <div className='HeroSection_decor'>
+                <div className='HeroSection_decor_imgWrapper'>
                     <img src="/img/heroImg.webp" alt="" />
                 </div>
             </div>
-        </div>
-        <div className='HeroSection_btn'>
-            <div className='HeroSection_btn_el'>
-                <img src="/img/shoto.svg" alt="" />
-                Buy now
+            <div className='HeroSection_btn'>
+                <div className='HeroSection_btn_el'>
+                    <img src="/img/shoto.svg" alt="" />
+                    Buy now
+                </div>
             </div>
         </div>
-    </div>
-)}
+    )
+}
