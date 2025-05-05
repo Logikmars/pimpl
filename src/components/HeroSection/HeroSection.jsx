61e78d72d2bcf9a087f2618307f4538f628eee07
logikmars <dima76673@gmail.com>
2025-05-05 15:33:56 +0300

    fix

--- src/components/HeroSection/HeroSection.jsx
+++ src/components/HeroSection/HeroSection.jsx
@@ -3,11 +3,9 @@ export default () => {return (
     <div className='HeroSection container'>
         <div className='HeroSection_decor'>
             <div className='HeroSection_decor_container free_img'>
-                {/* <div className='HeroSection_decor_black_Top HeroSection_decor_black free_img'></div> */}
                 <div className='HeroSection_decor_img free_img'>
                     <img src="/img/heroImg.webp" alt="" />
                 </div>
-                {/* <div className='HeroSection_decor_black_Bottom HeroSection_decor_black free_img'></div> */}
             </div>
         </div>
         <div className='HeroSection_btn'>
